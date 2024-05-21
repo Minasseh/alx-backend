9be1589d11b06fc0212e12a1095f988b7290c3af
root <mnsalaro@yahoo.com>
2024-05-21 05:34:06 -0700

    Fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" A FIFO caching system Class """
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ A class BasicCache that inherits from BaseCaching
+    and is a caching system """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key, _ = next(iter(self.cache_data.items()))
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
